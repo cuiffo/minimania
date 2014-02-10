8bfbc51746e8d86b774afeff1b4fbdc857c7d617
Eric Cuiffo <ecuiffo@gmail.com>
2014-02-10 08:55:24 -0500

    Revert "Pushed more files"
    This reverts commit 7b5e684c6423c0dd068a491a011222381c43716f.

--- platforms/android/CordovaLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/eric/minimania/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
-/home/eric/minimania/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/CordovaLib/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/eric/minimania/platforms/android/CordovaLib/bin/AndroidManifest.xml \
