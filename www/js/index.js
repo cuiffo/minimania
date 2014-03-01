52a3072951771d83d88811c4900df938a76c76f0
Nick Palumbo <shmozer@gmail.com>
2014-03-01 18:49:42 -0500

    added a ball visualization of accel

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        alert("HEY HEY");
         app.receivedEvent('deviceready');
     },
     
